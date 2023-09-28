984d3e555830f2ad7c01822019e31432d5dc177d
Diyorbek Mamadaliev <mamadaliev.dr19@gmail.com>
2023-09-28 13:57:05 +0500

    Added some improvements (Unit test)

---
+++ src/test/java/com/epam/automation/PastebinPageTest.java
@@ -0,0 +1,50 @@
+package com.epam.automation;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PastebinPageTest {
+
+    private WebDriver driver;
+    private PastebinPage pastebinPage;
+
+    @BeforeEach
+    void setUp() {
+        // Create a new WebDriver instance
+        driver = new ChromeDriver();
+
+        // Set a 10-second implicit wait
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        // Instantiate the PastebinPage class
+        pastebinPage = new PastebinPage(driver);
+    }
+
+    @AfterEach
+    void tearDown() {
+        // close the WebDriver after each test
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    void createNewPasteWithValidData() {
+        // Test with valid data
+        String code = "This is a test code.";
+        String expiration = "10 Minutes";
+        String name = "Test Paste";
+
+        pastebinPage.createNewPaste(code, expiration, name);
+
+        String expectedTitle = "Test Paste - Pastebin.com";
+        assertEquals(expectedTitle, driver.getTitle());
+
+    }
+}
