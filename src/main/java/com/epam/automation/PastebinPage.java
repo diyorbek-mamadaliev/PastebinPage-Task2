b87f66b99f11bbc4523dc8148a9509603a54b57b
Diyorbek Mamadaliev <mamadaliev.dr19@gmail.com>
2023-10-03 20:45:05 +0500

    Finished

--- src/main/java/com/epam/automation/PastebinPage.java
+++ src/main/java/com/epam/automation/PastebinPage.java
@@ -12,40 +12,39 @@ public class PastebinPage {
 
     private final WebDriver driver;
 
+    private final By codeFieldLocator = By.xpath("//*[@id='postform-text']");
+    private final By expirationDropdownLocator = By.id("select2-postform-expiration-container");
+    private final By highlightDropdownLocator = By.id("select2-postform-format-container");
+    private final By nameFieldLocator = By.id("postform-name");
+    private final By submitButtonLocator = By.xpath("//button[@class='btn -big']");
+
     public PastebinPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public void createNewPaste(String code, String expiration, String highlight, String name) {
-
         driver.get("https://pastebin.com/");
 
-
         WebElement codeField = new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='postform-text']")));
+                .until(ExpectedConditions.visibilityOfElementLocated(codeFieldLocator));
         codeField.sendKeys(code);
 
-
         WebElement expirationDropdown = new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.elementToBeClickable(By.id("select2-postform-expiration-container")));
+                .until(ExpectedConditions.elementToBeClickable(expirationDropdownLocator));
         expirationDropdown.click();
         driver.findElement(By.xpath("//li[text()='" + expiration + "']")).click();
 
-
         WebElement highlightDropdown = new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.elementToBeClickable(By.id("select2-postform-format-container")));
+                .until(ExpectedConditions.elementToBeClickable(highlightDropdownLocator));
         highlightDropdown.click();
         driver.findElement(By.xpath("//li[text()='" + highlight + "']")).click();
 
-
-
         WebElement nameField = new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.visibilityOfElementLocated(By.id("postform-name")));
+                .until(ExpectedConditions.visibilityOfElementLocated(nameFieldLocator));
         nameField.sendKeys(name);
 
-
         WebElement submitButton = new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='w0']/div[5]/div[1]/div[10]/button")));
+                .until(ExpectedConditions.elementToBeClickable(submitButtonLocator));
         submitButton.click();
     }
 }
