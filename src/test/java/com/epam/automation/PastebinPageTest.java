cc4866fb6a99b33565acee2951fa137aac16345a
Diyorbek Mamadaliev <mamadaliev.dr19@gmail.com>
2023-10-05 05:57:24 +0500

    Modified and fixed test script, used Testng instead of junit

--- src/test/java/com/epam/automation/PastebinPageTest.java
+++ src/test/java/com/epam/automation/PastebinPageTest.java
@@ -1,45 +1,59 @@
 package com.epam.automation;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-class PastebinPageTest {
+import java.time.Duration;
 
+public class PastebinPageTest {
     private WebDriver driver;
     private PastebinPage pastebinPage;
 
-    @BeforeEach
-    void setUp() {
-
+    @BeforeMethod
+    public void setUp() {
         driver = new ChromeDriver();
-
-
         pastebinPage = new PastebinPage(driver);
     }
 
-    @AfterEach
-    void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
     @Test
-    void createNewPasteWithValidData() {
-        // Test with valid data
-        String code = "This is a test code.";
+    public void testCreateNewPaste() {
+        String code = """
+                git config --global user.name  "New Sheriff in Town"
+                git reset $(git commit-tree HEAD^{tree} -m "Legacy code")
+                git push origin master --force""";
         String expiration = "10 Minutes";
         String highlight = "Bash";
-        String name = "Test Paste";
+        String name = "how to gain dominance among developers";
 
         pastebinPage.createNewPaste(code, expiration, highlight, name);
 
-        String expectedTitle = "Test Paste - Pastebin.com";
-        assertEquals(expectedTitle, driver.getTitle());
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
+        // Assertion 1: Verify the page title
+        WebElement titleElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h1[text()='how to gain dominance among developers']")));
+        Assert.assertTrue(titleElement.isDisplayed(), "Page title does not match.");
+
+        // Assertion 2: Verify syntax highlighting
+        WebElement syntaxHighlightingElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[text()='Bash']")));
+        Assert.assertTrue(syntaxHighlightingElement.isDisplayed(), "Syntax is not suspended for Bash.");
+
+        // Assertion 3: Verify saved code
+        WebElement codeElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//ol[@class='bash']")));
+        String savedCode = codeElement.getText();
+        Assert.assertEquals(savedCode, code, "Saved code does not match.");
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
     }
 }
